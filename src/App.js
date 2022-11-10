3031af399fae678389dbcddd9a7c2c587d71f9f9
minhtri2908 <91834683+minhtri2908@users.noreply.github.com>
2022-11-10 20:43:14 +0700

    FE

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './styles/css/bootstrap.min.css';
+import './styles/css/App.css';
+import Login from './page/login/login';
+import Home from './page/home/home';
+import Regishotel from './page/register-hotel/regishotel';
+import { useContext } from 'react';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+function App() {
+  return (
+    <main className="App">
+      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/register-hotel" element = {<Regishotel />} ></Route>
+        </Routes>
+      </BrowserRouter>,
+    </main>
+  );
+}
+
+export default App;
